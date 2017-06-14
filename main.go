4d56fab64b6d6b4ba15c622724e989af074a5920
A Yong <andraceyong@gmail.com>
2017-06-14 15:00:11 -0400

    commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+	"net/http")
+
+
+func main() {
+
+	mux :=http.NewServeMux()
+	filesHandler :=http.FileServer("public")
+	mux.Handle("/static/", http.StripPrefix("/static/", filesHandler))
+
+	/*
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/signup", signup)
+	*/
+	server :=&http.Server{
+		Addr: "0.0.0.0:7777",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
